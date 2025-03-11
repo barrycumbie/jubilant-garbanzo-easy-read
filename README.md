406cd034bb0dca43eb8d374abd40a4aaebb8630e
Barry <barrycumbie@gmail.com>
2025-03-11 10:33:00 -0500

    works on my box

---
+++ README.md
@@ -0,0 +1,1 @@
+reading data for dev easy 🦭
---
+++ scripts/readEasyData.js
@@ -0,0 +1,9 @@
+import {funFactsArray} from 'https://barrycumbie.github.io/super-system-easy-data/funFactsArray.js';
+
+$(function(){
+  
+  console.log('doc is ready!'); 
+
+  console.log(funFactsArray); 
+
+});
